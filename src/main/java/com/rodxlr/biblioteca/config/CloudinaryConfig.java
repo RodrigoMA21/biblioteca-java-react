f358fd4ab1fff36b2c0b763f900767409a69ba64
Rodrigo Mayer Alves <rodxlr@gmail.com>
2026-01-23 17:31:12 -0300

    Correção no CloudinaryConfig

--- src/main/java/com/rodxlr/biblioteca/config/CloudinaryConfig.java
+++ src/main/java/com/rodxlr/biblioteca/config/CloudinaryConfig.java
@@ -1,6 +1,5 @@
 package com.rodxlr.biblioteca.config;
 
-
 import com.cloudinary.Cloudinary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +12,12 @@ public class CloudinaryConfig {
 
     @Bean
     public Cloudinary cloudinary() {
+
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "SEU_CLOUD_NAME");
-        config.put("api_key", "SEU_API_KEY");
-        config.put("api_secret", "SEU_API_SECRET");
+        config.put("cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"));
+        config.put("api_key", System.getenv("CLOUDINARY_API_KEY"));
+        config.put("api_secret", System.getenv("CLOUDINARY_API_SECRET"));
+
         return new Cloudinary(config);
     }
 }
