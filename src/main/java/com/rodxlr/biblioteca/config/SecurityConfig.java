e02e5988b27548a3ac50f8c4b7de0c86535068f8
Rodrigo Mayer Alves <rodxlr@gmail.com>
2026-01-23 14:47:41 -0300

    feat: libera endpoint público para download de PDFs

--- src/main/java/com/rodxlr/biblioteca/config/SecurityConfig.java
+++ src/main/java/com/rodxlr/biblioteca/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**","/uploads/**").permitAll()
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**","/uploads/**", "/livros/pdf/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
