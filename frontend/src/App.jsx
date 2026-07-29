0b54625f8079406efe38c946c4e4677ea186f0f2
Rodrigo Mayer Alves <rodxlr@gmail.com>
2026-07-28 22:18:15 -0300

    fix: restaura BookCard.jsx corrompido e finaliza migracao para HashRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import DashboardLayout from "./components/layout/DashboardLayout";
 import Login from "./pages/Login";
@@ -37,10 +37,10 @@ function AppContent() {
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <AuthProvider>
         <AppContent />
       </AuthProvider>
-    </Router>
+    </HashRouter>
   );
 }
